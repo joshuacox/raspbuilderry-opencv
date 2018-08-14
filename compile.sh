cc6f86ce32fa535baa64144572a440bc98e8d2b1
Josh Cox <josh@webhosting.coop>
2018-08-14 12:36:59 -0500

    32

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ cd opencv
 git checkout 3.3.1
 mkdir -p build
 cd build
-make -j`nproc`
+#make -j`nproc`
+make -j32
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
